710d760c2e13a7574afd0a7eb08b8e302e86dec1
Dorian Henault <dorian.henault@etu.unice.fr>
2020-01-29 12:01:47 +0100

    Added an accessor to pins in KeyboardSensor

--- kernel/src/main/java/fr/unice/polytech/si5/dsl/structure/KeyboardSensor.java
+++ kernel/src/main/java/fr/unice/polytech/si5/dsl/structure/KeyboardSensor.java
@@ -2,7 +2,14 @@ package fr.unice.polytech.si5.dsl.structure;
 
 import fr.unice.polytech.si5.dsl.generator.Visitor;
 
+import java.util.List;
+
 public class KeyboardSensor extends Sensor<KeyboardSensor> {
+
+    public KeyboardSensor setPins(List<Integer> pins) {
+        super.pins = pins;
+        return this;
+    }
     @Override
     @SuppressWarnings("rawtypes")
     public void accept(Visitor visitor) {
